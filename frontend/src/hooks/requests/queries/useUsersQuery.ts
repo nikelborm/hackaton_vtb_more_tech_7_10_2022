9fddb50b82aacce6ce2eaebc33cbc6fde3be1d41
nikelborm <r0075301net.r@yandex.ru>
2022-10-09 09:39:18 +0300

    asd

--- frontend/src/hooks/requests/queries/useUsersQuery.ts
+++ frontend/src/hooks/requests/queries/useUsersQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import { customFetch } from 'utils';
 import { FindManyUsersResponseDTO } from 'backendTypes';
 
-export function useAllTags() {
+export function useAllUsersQuery() {
   const { isLoading, isError, isSuccess, data } = useQuery('useAllUsers', () =>
     customFetch<FindManyUsersResponseDTO>('user/all', {
       method: 'GET',
